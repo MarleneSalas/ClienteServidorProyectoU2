b083be92038d66b2483461aca3a3ce63ec03bdd6
Marlene Abigail Salas Guerrero <marlene.salas2218@gmail.com>
2024-04-12 01:14:31 -0600

    Cambios de diseño

---
+++ Views/HistorialMensajesView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClienteServidorProyectoU2.Views
+{
+    /// <summary>
+    /// Lógica de interacción para HistorialMensajesView.xaml
+    /// </summary>
+    public partial class HistorialMensajesView : UserControl
+    {
+        public HistorialMensajesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
